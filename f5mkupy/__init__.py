f4c061cd99d247fd008b9248735731839bd69201
Simon Kowallik <sk-github@simonkowallik.com>
2022-11-13 15:52:17 +0100

    version: 1.0.1

--- f5mkupy/__init__.py
+++ f5mkupy/__init__.py
@@ -9,7 +9,7 @@ __all__ = [
 ]
 __author__ = """Simon Kowallik"""
 __email__ = "github@simonkowallik.com"
-__version__ = "1.0.0"  # pyproject.toml
+__version__ = "1.0.1"  # pyproject.toml
 __projectname__ = "f5mkupy"
 # pylint: disable=line-too-long
 __description__ = "f5mkupy allows to encrypt and decrypt data using the format used in F5 BIG-IP bigip*.conf files with the key retrieved by f5mku -K."
